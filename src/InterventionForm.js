7a2755ad2d412703a1ae85ca5802b57b06badad9
Aymane KA <me@yahoo.fr>
2024-06-11 12:40:23 +0200

    Fixing id issue 1.7

--- src/InterventionForm.js
+++ src/InterventionForm.js
@@ -84,7 +84,7 @@ const InterventionForm = () => {
     setError(false);
 
     const data = new FormData();
-    formData.append('equipement_id', equipmentId); // Use equipmentId from context
+    data.append('equipment_id', equipmentId); // Use equipmentId from context
     Object.keys(formData).forEach(key => {
       if (key === 'files') {
         formData[key].forEach(file => {
